7a58b4fbcb90c1e86cd294e314cf44d87b1e2cd9
Hendi Santika <hendisantika@gmail.com>
2019-11-09 06:40:57 +0700

    #23 - Upgraded into Spring Boot 2.1.9.RELEASE version & add adjustment

--- src/main/java/com/hendi/webapp/SpringBootDemoApplication.java
+++ src/main/java/com/hendi/webapp/SpringBootDemoApplication.java
@@ -1,24 +1,17 @@
 package com.hendi.webapp;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import com.hendi.webapp.service.EmployeeServiceInterface;
 
 @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @ComponentScan("com.hendi.webapp")
 public class SpringBootDemoApplication {
-
-	@Autowired
-	EmployeeServiceInterface employeeServiceInterface;
-	
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
-   
    
 }
 
--- src/main/java/com/hendi/webapp/service/EmployeeServiceImplementation.java
+++ src/main/java/com/hendi/webapp/service/EmployeeServiceImplementation.java
@@ -2,56 +2,55 @@ package com.hendi.webapp.service;
 
 import com.hendi.webapp.domain.Employee;
 import com.hendi.webapp.repository.EmployeeRepository;
-import java.util.Collection;
-
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.Optional;
 
 
 @Service
 @Transactional
-public class EmployeeServiceImplementation implements EmployeeServiceInterface{
+public class EmployeeServiceImplementation implements EmployeeServiceInterface {
 
-	@Autowired
-	protected EmployeeRepository employeeRepository;
+    @Autowired
+    protected EmployeeRepository employeeRepository;
 
-	@Override
-	public Employee saveEmployee(Employee emp) {
-		// TODO Auto-generated method stub
-		return employeeRepository.save(emp);
-	}
+    @Override
+    public Employee saveEmployee(Employee emp) {
+        // TODO Auto-generated method stub
+        return employeeRepository.save(emp);
+    }
 
-	@Override
-	public Boolean deleteEmployee(String empId) {
-		// TODO Auto-generated method stub
-		Employee temp = employeeRepository.findOne(empId);
-		if(temp!=null){
-			 employeeRepository.delete(temp);
-			 return true;
-		}
-		return false;
-	}
+    @Override
+    public Boolean deleteEmployee(String empId) {
+        // TODO Auto-generated method stub
+        Optional<Employee> temp = employeeRepository.findById(empId);
+        if (temp.isPresent()) {
+            employeeRepository.delete(temp.get());
+            return true;
+        }
+        return false;
+    }
 
-	@Override
-	public Employee editEmployee(Employee emp) {
-		// TODO Auto-generated method stub
-		return employeeRepository.save(emp);
-	}
+    @Override
+    public Employee editEmployee(Employee emp) {
+        // TODO Auto-generated method stub
+        return employeeRepository.save(emp);
+    }
 
-	@Override
-	public Collection<Employee> getAllEmployees() {
-		// TODO Auto-generated method stub
-		Iterable<Employee> itr = employeeRepository.findAll();
-		return (Collection<Employee>)itr;
-	}
+    @Override
+    public Collection<Employee> getAllEmployees() {
+        // TODO Auto-generated method stub
+        Iterable<Employee> itr = employeeRepository.findAll();
+        return (Collection<Employee>) itr;
+    }
 
-	@Override
-	public Employee findEmployee(String empId) {
-		// TODO Auto-generated method stub
-		return employeeRepository.findOne(empId);
-	}
-	
+    @Override
+    public Employee findEmployee(String empId) {
+        return employeeRepository.findById(empId).orElseThrow(null);
+    }
+
 
 }
